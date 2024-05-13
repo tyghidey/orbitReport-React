31c6e711dfc794d67a6304d89975ca8fe42164f7
Guest <StoppingBy@Nahomis-MacBook-Pro-2.local>
2024-05-13 08:26:12 -0500

    completed

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import "./styling.css";
+
+function Banner() {
+  return (
+    <>
+      <header>Orbit Report</header>
+      <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+  );
+}
+
+export default Banner;
